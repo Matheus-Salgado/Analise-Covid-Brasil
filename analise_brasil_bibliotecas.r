ccddd92460d4aa39d30285124545295ff6d55c26
unknown <matheus98gustavo@gmail.com>
2021-08-27 13:40:35 -0300

    analise com perguntas e explicacoes

--- analise_brasil_bibliotecas.r
+++ analise_brasil_bibliotecas.r
@@ -2,6 +2,7 @@ library(lubridate)
 library(ggplot2)
 library(dplyr)
 library(magrittr)
+library(plotly)
 
 ler_dados_csv <- function(arquivo){
   dados <- read.csv(file = arquivo)
@@ -10,7 +11,47 @@ ler_dados_csv <- function(arquivo){
 
 dados_completos <- ler_dados_csv("C:/Users/matheus/Desktop/Projeto_Covid_em_R/datasets/owid-covid-data.csv")
 
-dados_completos <- as_tibble(dados_completos)
+View(dados_completos)
+summary(dados_completos)
+
+# quais são os 20 países que mais pessoas morreram (valor bruto)?
+
+dados_completos %>%
+  group_by(location) %>%
+  mutate(new_deaths = new_deaths/1000) %>%
+  filter(!location %in% c("World", "Europe", "South America", "European Union", "Asia", "North America")) %>%
+  summarise(total_mortes = sum(new_deaths, na.rm = TRUE)) %>%
+  arrange(desc(total_mortes)) %>%
+  head(20) %>%
+  ggplot() +
+  geom_col(aes(x = total_mortes, y = reorder(location, total_mortes)), fill = "blue") +
+  labs(title = "Países com mais mortos devido ao COVID-19 - valores brutos", x = "mortes em milhares", y = "países") +
+  theme_bw() +
+  theme(axis.text = element_text(size = 12), axis.title = element_text(face = "bold"), 
+        plot.title = element_text(size = 12, face = "bold", color = "black"))
+
+# quais são os 20 países que mais pessoas morreram (valor por milhão)?
+
+dados_completos %>%
+  group_by(location) %>%
+  filter(!location %in% c("World", "Europe", "South America", "European Union", "Asia", "North America")) %>%
+  summarise(total_mortes = sum(new_deaths_per_million, na.rm = TRUE)) %>%
+  arrange(desc(total_mortes)) %>%
+  head(20) %>%
+  ggplot() +
+  geom_col(aes(x = total_mortes, y = reorder(location, total_mortes)), fill = "blue") +
+  labs(title = "Países com mais mortos devido ao COVID-19 - valores por milhão", x = "mortes por milhão", y = "países") +
+  theme_bw() +
+  theme(axis.text = element_text(size = 12), axis.title = element_text(face = "bold"), 
+        plot.title = element_text(size = 12, face = "bold", color = "black"))
+ 
+# temos que, ao analisar os valores brutos, o brazil está na segunda posição em vítimas devido ao COVID-19, esta posição pode ser
+# explicada levando em consideração o peso da população brasileira.
+# Já nos dados por milhão o brazil cai para a 5° posição, mas, apesar de ter caido algumas posições, mostra a situação complicada do país,
+# dado que os estados unidos, que possui um "peso" devido à população semelhante ao brazil, sequer está nas primeiras 20 posições
+ 
+
+ #######################proxima pergunta#######################################
 
 dados_completos$date <- as.Date(dados_completos$date)
 class(dados_completos$date)

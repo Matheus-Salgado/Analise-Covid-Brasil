d459de0a759635ab2045a1958d74d11e0cf5cea8
unknown <matheus98gustavo@gmail.com>
2021-10-13 12:10:45 -0300

    organizando o codigo

--- analise_brasil_bibliotecas.r
+++ analise_brasil_bibliotecas.r
@@ -4,6 +4,7 @@ library(dplyr)
 library(magrittr)
 library(plotly)
 library(grid)
+library(patchwork)
 
 ler_dados_csv <- function(arquivo){
   dados <- read.csv(file = arquivo)
@@ -14,9 +15,6 @@ dados_completos <- ler_dados_csv("C:/Users/matheus/Desktop/Projeto_Covid_em_R/da
 dados_completos_vacinacao <- ler_dados_csv("C:/Users/matheus/Desktop/Projeto_Covid_em_R/datasets/VaccinationData.csv")
 dados_completos$date <- as.Date(dados_completos$date)
 dados_completos_vacinacao$date <- as.Date(dados_completos_vacinacao$date)
-
-
-
 
 View(dados_completos)
 summary(dados_completos)
@@ -36,7 +34,7 @@ dados_completos %>%
   theme_bw() +
   theme(axis.text = element_text(size = 12), axis.title = element_text(face = "bold"), 
         plot.title = element_text(size = 12, face = "bold", color = "black")) +
-  geom_text(aes(label = total_mortes))
+  geom_text(aes(label = total_mortes, x = total_mortes+40))
 
 
 
@@ -54,7 +52,7 @@ dados_completos %>%
   theme_bw() +
   theme(axis.text = element_text(size = 12), axis.title = element_text(face = "bold"), 
         plot.title = element_text(size = 12, face = "bold", color = "black")) +
-  geom_text(aes(label = total_mortes))
+  geom_text(aes(label = total_mortes, x = total_mortes+600))
  
 # temos que, ao analisar os valores brutos, o brazil está na segunda posição em vítimas devido ao COVID-19, esta posição pode ser
 # explicada levando em consideração o peso da população brasileira.
@@ -67,7 +65,8 @@ dados_completos %>%
 # quais são os países que mais aplicaram dose?
 
 dados_completos %>%
-  filter(!location %in% c("World", "Europe", "South America", "European Union", "Asia", "North America", "China")) %>%
+  filter(!location %in% c("World", "Europe", "South America", "European Union", "Asia", "North America", "China") &
+         !is.na(total_vaccinations)) %>%
   group_by(location) %>%
   select(location, total_vaccinations) %>%
   summarise(total_doses = max(total_vaccinations, na.rm = TRUE)) %>%
@@ -83,7 +82,8 @@ dados_completos %>%
 # quais são os países que mais têm pessoas totalmente vacinadas?
 
 dados_completos %>%
-  filter(!location %in% c("World", "Europe", "South America", "European Union", "Asia", "North America", "China")) %>%
+  filter(!location %in% c("World", "Europe", "South America", "European Union", "Asia", "North America", "China") &
+         !is.na(people_fully_vaccinated)) %>%
   group_by(location) %>%
   select(location, people_fully_vaccinated) %>%
   summarise(pessoas_vacinacao_completa = max(people_fully_vaccinated, na.rm = TRUE)) %>%
@@ -134,46 +134,8 @@ ggplotly(grafico_vacinacao_brazil_usa)
 # nao vacinados
 
 
-
 ################################################################################
   
-
-
-dados_completos$date <- as.Date(dados_completos$date)
-class(dados_completos$date)
-
-sum(is.na(dados_completos$date))
-sum(is.na(dados_completos$new_deaths))
-sum(is.na(dados_completos$continent))
-
-dados_completos$new_deaths[is.na(dados_completos$new_deaths)] <- 0
-
-dados_completos %>%
-  select(date,location, new_deaths) %>%
-  filter(location == "Brazil") %>%
-  ggplot() +
-  geom_line(aes(x = date, y = new_deaths), color = "blue")
-
-  
-dados_completos %>%
-  filter(location == "Brazil") %>%
-  summarize(
-    media = mean(new_deaths),
-    mediana = median(new_deaths)
-)
-
-dados_completos %>%
-  select(location, new_deaths) %>%
-  filter(location %in% c("Brazil", "United States", "Russia", "Mexico", "India")) %>%
-  group_by(location) %>%
-  summarize(media_mortes = mean (new_deaths)) %>%
-  ggplot() +
-  geom_col(aes(x = reorder(location,-media_mortes), y = media_mortes), color = "blue", fill = "blue") +
-  labs(title = "Média das mortes diárias por país", x = "país", y = "valor da média das mortes diárias") +
-  theme_bw() +
-  theme(axis.text = element_text(size = 14), axis.title = element_text(size = 8),
-        plot.title = element_text(size = 20, face = "bold", color = "darkblue"))
-
 
 
 
